1ed0c07d2ceeee0ae6336e51aa730f18cffed70c
Habibur Rahman <hbrawnak@gmail.com>
2022-03-05 21:18:35 +0600

    Photo list in grid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className='photo-grid'>
-                im photo
+                {this.props.posts.map((post, index) =>
+                    <Photo {...this.props} post={post} index={index} key={index}/>
+                )}
             </div>
         );
     }
