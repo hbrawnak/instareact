165dbacd1703b38a426de61542add2c95b958520
Habibur Rahman <hbrawnak@gmail.com>
2022-03-06 21:45:56 +0600

    implemented react reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,15 @@
 function posts(state = [], action) {
-    console.log('post will change')
-    console.log(state, action);
-    return state;
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            const index = action.index;
+            return [
+                ...state.slice(0, index), //before the one updating
+                {...state[index], likes: state[index].likes + 1},
+                ...state.slice(index + 1), //after the one updating
+            ]
+        default:
+            return state
+    }
 }
 
 export default posts;
