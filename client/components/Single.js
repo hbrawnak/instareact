5d2606203852e08830db9469a24bcfea187a9dd1
Habibur Rahman <hbrawnak@gmail.com>
2022-03-06 21:58:52 +0600

    Single photo page

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
     render() {
+        const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+        const post = this.props.posts[index];
+        console.log(post);
         return (
             <div className='single-photo'>
-                im single
+                <Photo {...this.props} post={post} index={index}/>
             </div>
         );
     }
